c587a205040033ee0b7426f0bee241068cd306b2
Jonathan Hodges <jonathan.hodges@manheim.com>
2017-03-23 16:02:59 -0400

    added start of tic tac toe with endpoint

--- hello_world_ruby/hello_world.rb
+++ hello_world_ruby/hello_world.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
+require 'json'
 
 environment = ENV['ENVIRONMENT']
 
 get '/hello' do
   'hello world!'
 end
+
+post '/tictactoe/next-move' do
+#  "*********"
+  request.body.rewind
+  payload = JSON.parse(request.body.read)
+  board = payload["board"]
+  board.sub!(/\*/, payload["piece"])
+  board
+end
+
+
+
